016ca86e9094fef2a73341edf27bc22765336d94
yemiwebby <yemiwebby@gmail.com>
2023-10-21 16:01:18 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to the Rust API"
+}
+
+#[get("/greet/<name>")]
+fn greet(name: String) -> String {
+    format!("Hey {}, glad to have you here!", name)
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index,greet]).launch();
+}
